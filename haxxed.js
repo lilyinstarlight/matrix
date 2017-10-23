2981f39254e967afdb4833160a80eff120580c13
Foster McLane <fkmclane@gmail.com>
2017-10-23 19:57:12 -0400

    add turn-off animation

--- haxxed.js
+++ haxxed.js
@@ -1,4 +1,4 @@
-// Mostly taken from https://codepen.io/team/nclud/pen/MwaGGE
+// Mostly taken from https://codepen.io/team/nclud/pen/MwaGGE and https://codepen.io/lbebber/pen/XJRdrV
 
 var matrix = null;
 
@@ -6,54 +6,41 @@ var remove = function(element) {
 	element.parentElement.removeChild(element)
 }
 
-var copy = function(canvas) {
-	var image = new Image();
-	image.src = canvas.toDataURL();
-
-	var out = document.createElement('canvas');
-	out.width = canvas.width;
-	out.height = canvas.height;
-
-	var context = out.getContext('2d');
-	context.drawImage(image, 0, 0);
-
-	return out;
-}
-
-var view = function(element) {
+var view = function(element, over) {
 	element.style.width = window.innerWidth;
 	element.style.height = window.innerHeight;
 	element.style.position = 'fixed';
 	element.style.top = 0;
 	element.style.left = 0;
-	element.style.zIndex = 31337;
+	element.style.zIndex = 31337 + over;
 }
 
 var capture = function() {
 	html2canvas(document.body, {
 		'onrendered': function(canvas) {
-			animate(canvas.toDataURL(), function(gcanvas) {
-				var mcanvas = copy(gcanvas);
-				view(mcanvas);
+			glitch(canvas.toDataURL(), function(gcanvas) {
+				var mcanvas = document.createElement('canvas');
+				view(mcanvas, 0);
 				mcanvas.style.background = '#000';
 				document.body.append(mcanvas);
-				remove(gcanvas);
 
 				matrix = new Matrix(mcanvas);
 				matrix.start();
-				setTimeout(function() {
-					matrix.write('MESS WITH THE BEST\n DIE LIKE THE REST', (window.innerWidth - 215)/2, window.innerHeight*0.35, function() {
-						setTimeout(function() {
-							matrix.rain();
-						}, 6000);
-					});
-				}, 1400);
+
+				off(gcanvas, function() {
+					setTimeout(function() {
+						matrix.write('MESS WITH THE BEST\n DIE LIKE THE REST', (window.innerWidth - 215)/2, window.innerHeight*0.35);
+					}, 1400);
+				});
 			});
 		}
 	});
 };
 
-var animate = function(image, callback, rate, glitchParams, delta) {
+var glitch = function(image, callback, frames, rate, glitchParams, delta) {
+	if (typeof frames === 'undefined')
+		frames = 100;
+
 	if (typeof rate === 'undefined')
 		rate = 100;
 
@@ -63,7 +50,7 @@ var animate = function(image, callback, rate, glitchParams, delta) {
 	if (typeof delta === 'undefined')
 		delta = 0.1;
 
-	var renderer, composer1, composer2;
+	var renderer, composer1, composer2, renderGlitch;
 
 	var init = function() {
 		var scene = new THREE.Scene();
@@ -76,51 +63,43 @@ var animate = function(image, callback, rate, glitchParams, delta) {
 			'map': THREE.ImageUtils.loadTexture(image),
 			'depthTest': false
 		});
-
 		background.map.needsUpdate = true;
-		var plane = new THREE.PlaneBufferGeometry(1, 1);
 
+		var plane = new THREE.PlaneBufferGeometry(1, 1);
 		var bgMesh = new THREE.Mesh(plane, background);
 		bgMesh.position.z = 1;
 		bgMesh.scale.set(window.innerWidth, window.innerHeight, 1);
-		sceneBg.add(bgMesh);
-
 		bgMesh.material.map.needsUpdate = true;
-
-		var sceneMask = new THREE.Scene();
+		sceneBg.add(bgMesh);
 
 		renderer = new THREE.WebGLRenderer();
 		renderer.setClearColor(0xffffff);
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.setPixelRatio(window.devicePixelRatio);
 		renderer.autoClear = false;
-
 		renderer.gammaInput = true;
 		renderer.gammaOutput = true;
 
-		renderBackground = new THREE.RenderPass(sceneBg, camera);
 
-		view(renderer.domElement);
+		view(renderer.domElement, 1);
 		document.body.append(renderer.domElement);
 
 		var targetParams = {'minFilter': THREE.LinearFilter, 'magFilter': THREE.LinearFilter, 'format': THREE.RGBFormat, 'stencilBuffer': true};
 
-		var clearMask = new THREE.ClearMaskPass();
-
 		composer1 = new THREE.EffectComposer(renderer, new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, targetParams));
-		var renderScene = new THREE.TexturePass(composer1.renderTarget2);
+		var renderBackground = new THREE.RenderPass(sceneBg, camera);
+		var clearMask = new THREE.ClearMaskPass();
 		composer1.addPass(renderBackground);
 		composer1.addPass(clearMask);
 
 		composer2 = new THREE.EffectComposer(renderer, new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight, targetParams));
-
-		var renderGlitch = new THREE.GlitchPass(glitchParams['size'], glitchParams['delay'], glitchParams['amplification']);
+		var renderScene = new THREE.TexturePass(composer1.renderTarget2);
+		renderScene.uniforms['tDiffuse'].value = composer1.renderTarget2;
+		renderGlitch = new THREE.GlitchPass(glitchParams['size'], glitchParams['delay'], glitchParams['amplification']);
+		renderGlitch.goWild = true;
 		renderGlitch.renderToScreen = true;
-
 		composer2.addPass(renderScene);
 		composer2.addPass(renderGlitch);
-
-		renderScene.uniforms[ 'tDiffuse' ].value = composer1.renderTarget2;
 	}
 
 	var render = function() {
@@ -129,25 +108,33 @@ var animate = function(image, callback, rate, glitchParams, delta) {
 		composer2.render(delta);
 	}
 
-	var glitch = true;
-
 	var step = function() {
-		if (glitch) {
+		if (frames > 0) {
+			frames--;
 			setTimeout(function() {
 				render();
 				requestAnimationFrame(step);
 			}, rate);
 		}
+		else {
+			renderGlitch.goWild = false;
+			callback && callback(renderer.domElement);
+		}
 	}
 
 	init();
 	step();
-
-	setTimeout(function() {
-		callback && callback(renderer.domElement);
-		glitch = false;
-	}, 3400);
 }
+
+var off = function(canvas, callback) {
+	canvas.addEventListener('animationend', function(ev) {
+		remove(canvas);
+
+		callback && callback();
+	}, false);
+
+	canvas.className += 'turn-off';
+};
 
 var load = function(list, callback) {
 	var script = document.createElement('script');
@@ -166,20 +153,46 @@ var load = function(list, callback) {
 load([
 	'https://gitcdn.link/repo/fkmclane/matrix/master/matrix.js',
 	'https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js',
+	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/141552/03_glitch.js',
+	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/141552/08_texturepass.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/CopyShader.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/EffectComposer.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/RenderPass.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/ShaderPass.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/MaskPass.js',
 	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-18/GlitchPass.js',
-	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/141552/03_glitch.js',
-	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/141552/08_texturepass.js',
 	'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js'
 ], function() {
 	var link = document.createElement('link');
 	link.rel = 'stylesheet';
 	link.href = 'https://gitcdn.link/repo/fkmclane/matrix/master/matrix.css';
 	document.head.append(link);
+
+	var style = document.createElement('style');
+	style.innerHTML = [
+		'@keyframes turn-off {',
+			'0% {',
+				'transform: scale(1,1.3) translate3d(0,0,0);',
+				'filter: brightness(1);',
+			'}',
+
+			'60% {',
+				'transform: scale(1.3,0.001) translate3d(0,0,0);',
+				'filter: brightness(10);',
+			'}',
+
+			'100% {',
+				'transform: scale(0.000,0.0001) translate3d(0,0,0);',
+				'filter: brightness(50);',
+			'}',
+		'}',
+
+		'.turn-off {',
+			'animation: turn-off 0.55s cubic-bezier(0.230, 1.000, 0.320, 1.000);',
+			'animation-fill-mode: forwards;',
+		'}'
+	].join('\n')
+	document.head.append(style);
 
 	capture();
 });
